23041311aa67a89c3a314943497fdc911d514976
Fabien Nicollet <fnicollet@gmail.com>
2019-10-14 11:26:54 +0200

    final demo app

--- orcarina-server/app/controllers/Application.java
+++ orcarina-server/app/controllers/Application.java
@@ -17,29 +17,32 @@ public class Application extends Controller {
     while (iterator.hasNext()) {
       String fullNote = iterator.next();
     //for (String fullNote : cache) {
-      String noteOnly = fullNote.substring(2, 3);
-      //Logger.info("fullNote: " + fullNote);
-      //Logger.info("noteOnly: " + noteOnly);
-      notes.add(noteOnly);
+      String noteOnly = fullNote.substring(1, 2);
+      String noteString;
+      if ("A".equalsIgnoreCase(noteOnly) || "2".equalsIgnoreCase(noteOnly)){
+        noteString = "1";
+      } else if ("B".equalsIgnoreCase(noteOnly) || "4".equalsIgnoreCase(noteOnly)){
+        noteString = "2";
+      } else if ("C".equalsIgnoreCase(noteOnly) || "6".equalsIgnoreCase(noteOnly)){
+        noteString = "3";
+      } else if ("D".equalsIgnoreCase(noteOnly) || "8".equalsIgnoreCase(noteOnly)){
+        noteString = "4";
+      } else {
+        noteString = "1";
+      }
+      notes.add(noteString);
     }
     String serial = StringUtils.join(notes, ";");
     renderText(serial);
   }
 
   public static void index() throws Exception {
-    ArrayList<String> messages = new ArrayList<String>(
-        Arrays.asList("A",
-            "B",
-            "C",
-            "D",
-            "E",
-            "F"));
-    String messagesSent = StringUtils.join(messages, ";");
+    //List<String> messages = dumpCache();
+    //messages = new ArrayList<>();
+    //messages.add("A");
     //sendToPilot(messages);
-
-    List<String> cache = dumpCache();
-
-    render(messagesSent, cache);
+    //String messagesSent = StringUtils.join(messages, ";");
+    render();
   }
 
   public static List<String> dumpCache(){
@@ -52,23 +55,30 @@ public class Application extends Controller {
 
   public static boolean sendToPilot(List<String> messages) {
 
-    int DELAY_SECONDS = 2;
     int size = messages.size();
     for (int i = 0; i < size; i++) {
       String message = messages.get(i);
+      message = message.substring(2, 3);
       message = "75" + message;
       Logger.info("Sending to Pilot: " + message);
       Bootstrap.sendUDPMessage(message);
       if (i != size - 1) {
+
         try {
-          Thread.sleep(DELAY_SECONDS * 1000);
+          Thread.sleep(2 * 100);
         } catch (InterruptedException e) {
           Logger.error("", e);
         }
+
       }
 
     }
     return false;
+  }
+
+  public static void master(){
+    List<String> cache = dumpCache();
+    render(cache);
   }
 
 
--- orcarina-server/app/jobs/Bootstrap.java
+++ orcarina-server/app/jobs/Bootstrap.java
@@ -11,48 +11,44 @@ import java.util.List;
 @OnApplicationStart(async=true)
 public class Bootstrap extends Job {
 
-  private boolean pause = false;
-  final Object    sync = new Object();
-
   public static final String MESSAGES_CACHE_KEY = "MESSAGES_CACHE_KEY";
 
   public void doJob() {
-    DatagramSocket serverSocket = null;
+    DatagramSocket serverSocket;
     try {
-      serverSocket = new DatagramSocket(49161 );
+      serverSocket = new DatagramSocket(49161);
       byte[] receiveData = new byte[1024];
       while(true)
       {
         DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
         serverSocket.receive(receivePacket);
-        String sentence = new String( receivePacket.getData());
-        Logger.info("RECEIVED FROM ORCA: " + sentence);
+        String message = new String( receivePacket.getData());
+        Logger.info("RECEIVED FROM ORCA: " + message);
         List<String> messages = (ArrayList<String>) Cache.get(MESSAGES_CACHE_KEY);
         if (messages == null){
           messages = new ArrayList<>();
         }
-        messages.add(0, sentence);
-        messages = new ArrayList<String>(messages.subList(0, Math.min(messages.size(), 16)));
+        messages.add(0, message);
+        messages = new ArrayList<>(messages.subList(0, Math.min(messages.size(), 16)));
         Cache.set(MESSAGES_CACHE_KEY, messages);
+
+        Bootstrap.sendUDPMessage(message);
 
       }
     } catch (Exception e) {
       Logger.error("Datagram error", e);
     }
-
-
-
   }
-
-
 
   public static void sendUDPMessage(String sentence){
     try {
       DatagramSocket clientSocket = new DatagramSocket();
-      InetAddress IPAddress = InetAddress.getByName("localhost");
+      //InetAddress IPAddress = InetAddress.getByName("localhost");
+      // Saly
       //InetAddress IPAddress = InetAddress.getByName("192.168.1.140");
+      // Toon
+      InetAddress IPAddress = InetAddress.getByName("192.168.1.126");
       byte[] sendData = new byte[1024];
-      byte[] receiveData = new byte[1024];
       sendData = sentence.getBytes();
        // 49161 is Pilot default UDP port
       DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 49161);
